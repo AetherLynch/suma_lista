53356deea9972f6fc94cded2067a50bc97ac776b
Uriel <royality73@gmail.com>
2026-03-03 16:37:17 -0700

    Modifique el test

--- tests/test_suma_lista.py
+++ tests/test_suma_lista.py
@@ -1,4 +1,9 @@
+import sys
+from pathlib import Path
 import pytest
+
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+
 from suma_lista import suma_lista
 
 
@@ -10,6 +15,10 @@ def test_caso_limite_lista_con_ceros():
     assert suma_lista([0, 0, 0]) == 0.0
 
 
+def test_caso_correcto_decimales():
+    assert suma_lista([1.5, 2.5]) == 4.0
+
+
 def test_caso_error_elemento_no_numerico():
     with pytest.raises(TypeError):
         suma_lista([1, "2", 3])
@@ -18,7 +27,3 @@ def test_caso_error_elemento_no_numerico():
 def test_caso_error_lista_vacia():
     with pytest.raises(ValueError):
         suma_lista([])
-
-
-def test_caso_correcto_decimales():
-    assert suma_lista([1.5, 2.5]) == 4.0
