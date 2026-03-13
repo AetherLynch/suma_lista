5b20a1ea22d0a45c40764c48a2dc1fb7b107dffa
AetherLynch <royality73@gmail.com>
2026-03-12 19:05:27 -0600

    Modifique el proyecto para Docker

--- app.py
+++ app.py
@@ -3,16 +3,12 @@ from suma_lista import suma_lista
 
 app = Flask(__name__)
 
-with open("index.html", "r", encoding="utf-8") as f:
-    HTML_TEMPLATE = f.read()
+with open("index.html", "r", encoding="utf-8") as archivo:
+    plantilla = archivo.read()
 
 
 @app.route("/")
-def home():
-    ejemplo = [1, 2, 3]
-    resultado = suma_lista(ejemplo)
-    return render_template_string(HTML_TEMPLATE, lista=ejemplo, resultado=resultado)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+def inicio():
+    lista = [1, 2, 3]
+    resultado = suma_lista(lista)
+    return render_template_string(plantilla, lista=lista, resultado=resultado)
